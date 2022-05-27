b6e8f9b1381fc9bfd44d03caf0024fd99e42512b
김하빈 <>
2022-05-27 09:58:00 +0900

    connection 수정

--- db/db.js
+++ db/db.js
@@ -1,6 +1,7 @@
 const mariadb = require('mariadb');
 const config = require('../config/db-config.json')
 
+//파라미터 json이 아니라 객체인듯 참고하세요
 const pool = mariadb.createPool({
   host: config.host, 
   port: config.port,
@@ -11,19 +12,22 @@ const pool = mariadb.createPool({
 });
 
 //미완성
-async function asyncFunction(sql) {
+async function asyncFunction(sql, callback) {
   let conn;
+  let result;
   console.log('test1')
   try {
 	  conn = await pool.getConnection();
-    console.log('test2')
-    return await conn.query(sql);
+    console.log('test2');
+    result = await conn.query(sql);
+    console.log(result);
+    return result;
   } catch (err) {
-    console.log('test6')
 	  throw err;
   } finally {
 	  if (conn) {
       console.log('test5')
+      console.log(result);
       return conn.end()
     };
   }
--- routes/menu.js
+++ routes/menu.js
@@ -5,8 +5,10 @@ var pool = require('../db/db');
 /* GET users listing. */
 router.get('/menuList', function(req, res, next) {
     console.log('test3')
-    console.log(pool('select * from menu'));
+    let result = pool('select * from menu');
+    console.log(result);
     console.log('test4')
+    console.log(result);
 });
 
 router.get('/addMenu', function(req, res, next) {
