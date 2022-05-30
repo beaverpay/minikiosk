1d468a9cb89310397df4b54030ee4b6fddbf40bd
Eomjihye12 <ojawl12044@gmail.com>
2022-05-30 14:28:44 +0900

    주문, 주문조회

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-//var orderRouter = require('./routes/order');
+var orderRouter = require('./routes/order');
 var menuRouter = require('./routes/menu');
 
 var app = express();
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-// app.use('/order', orderRouter);
+app.use('/order', orderRouter);
 app.use('/menu', menuRouter);
 
 // catch 404 and forward to error handler
---
+++ routes/order.js
@@ -0,0 +1,22 @@
+let express = require('express');
+let router = express.Router();
+let JSONbig = require('json-bigint');
+let excuteStatement = require('../db/db');
+
+router.get('/orderList', function(req, res, next) {
+  excuteStatement('SELECT orders.id, menu.menu_name, menu.menu_price, orders.order_amount, orders.order_amount * menu.menu_price AS order_total FROM education.orders LEFT JOIN education.menu ON orders.menu_id=menu.id',async function(orderList){
+    await res.json(JSON.parse(JSONbig.stringify(orderList)));
+  });
+});
+
+router.post('/registOrder', function(req, res, next) {
+  let params = req.body;
+  let paramArray = Object.values(params);
+  let values = [null,...paramArray];
+
+  excuteStatement('INSERT INTO orders values(?,?,?,?)', function(result){
+      res.send(JSON.parse(JSONbig.stringify(result)))
+  }, values);
+});
+
+module.exports = router;;
