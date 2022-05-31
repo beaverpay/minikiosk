8ece0fd30f19ce81b1233ada790ca4efc8d5a9cc
김하빈 <>
2022-05-31 13:43:16 +0900

    주업 작업

--- routes/store.js
+++ routes/store.js
@@ -2,14 +2,31 @@ var express = require('express');
 var router = express.Router();
 let authJWT = require('../middlewares/authJWT');
 let excuteStatement = require('../db/db');
-const bcrypt = require('bcrypt');
 let JSONbig = require('json-bigint');
 
-/* admin 토큰 인증 후 매니저 생성 */
+/* 매장의 이름과 지점을 받아 매장 id를 반환 */
+router.get('/read', async function(req, res, _next){
+    let store_name = req.query.store_name;
+    let store_branch = req.query.store_branch;
+    let values = [store_name, store_branch]
+    try{
+        let result = await excuteStatement('select store_id from store where store_name = ? and store_branch = ?', values);
+        res.status(200).send({
+            ok:true,
+            data: result[0]
+        })
+    }catch(err){
+        res.status(401).send({
+            ok:false,
+            message: err.message
+        });
+    }
+})
+
+/* admin 토큰 인증 후 매장 생성 */
 router.post('/create', authJWT, async function(req, res, _next) {
     let store_id = req.store_id;
     let role = req.role;
-    let result = null;
 
     let store_name = req.body.store_name;
     let store_branch = req.body.store_branch;
@@ -17,13 +34,13 @@ router.post('/create', authJWT, async function(req, res, _next) {
 
     try{
         if(store_id === 1 && role === 'admin'){
-        result = await excuteStatement('insert into store values(?,?,?,?)', [null, store_name, store_branch, store_tel])
+        let result = await excuteStatement('insert into store values(?,?,?,?)', [null, store_name, store_branch, store_tel])
         res.status(200).send({
             ok: true,
             data: JSON.parse(JSONbig.stringify(result))
         })
         }else{
-        throw new Error('권한이 없습니다.')
+            throw new Error('권한이 없습니다.')
         }
     }catch(err){
         res.status(401).send({
@@ -33,27 +50,31 @@ router.post('/create', authJWT, async function(req, res, _next) {
     }
 });
 
-/* admin 토큰 인증 후 매니저 삭제 */
+/* admin 토큰 인증 후 매장 삭제 */
 router.delete('/delete/:store_id', authJWT, async function(req, res, _next){
-  let store_id = req.store_id;
-  let role = req.role;
-  let result = null;
+    let store_id = req.store_id;
+    let role = req.role;
 
-  try{
-    if(store_id === 1 && role === 'admin'){
-      result = await excuteStatement('delete from store where store_id = ?', [req.params.store_id])
-      res.status(200).send({
-        ok: true,
-        data: JSON.parse(JSONbig.stringify(result))
-    })}else{
-      throw new Error('권한이 없습니다.')
+    try{
+        if(store_id === 1 && role === 'admin'){
+            let result = await excuteStatement('delete from store where store_id = ?', [req.params.store_id])
+            if(result.affectedRows > 0){
+                res.status(200).send({
+                  ok: true,
+                  data: JSON.parse(JSONbig.stringify(result))
+                })
+            }else{
+                throw new Error('존재하지 않는 매장 id입니다.')
+            }
+        }else{
+            throw new Error('권한이 없습니다.')
+        }
+    }catch(err){
+        res.status(401).send({
+        ok:false,
+        message: err.message
+        });
     }
-  }catch(err){
-    res.status(401).send({
-      ok:false,
-      message: err.message
-    });
-  }
 });
 
 module.exports = router;
