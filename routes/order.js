1251d05adde91f231a2bf681eda1dcbb65dc1352
Eomjihye12 <ojawl12044@gmail.com>
2022-06-08 11:33:14 +0900

    주문 수정

--- routes/order.js
+++ routes/order.js
@@ -2,13 +2,7 @@ const express = require('express');
 const OrderController = require('../controllers/OrderController');
 const router = express.Router();
 
-<<<<<<< Updated upstream
-router.get('/', OrderController.search);
-router.get('/storeId/:menu_store_id', OrderController.searchByStoreId);
-=======
 router.get('/:id', OrderController.search);
->>>>>>> Stashed changes
-
 router.post('/', OrderController.post);
 
 module.exports = router;
