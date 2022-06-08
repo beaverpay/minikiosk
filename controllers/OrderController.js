3c129c0b740c62e89f97c10953c5241d4201babf
김하빈 <>
2022-06-08 11:27:06 +0900

    오더

--- controllers/OrderController.js
+++ controllers/OrderController.js
@@ -16,9 +16,10 @@ module.exports = {
 		}
 	},
 	async searchByStoreId(req, res, next){
+		console.log(req.params.menu_store_id);
 		try {
 			result = await excuteStatement(
-				'select * from order where menu_store_id = ?',[req.params.menu_store_id]
+				'select * from orders where menu_store_id = ?',[req.params.menu_store_id]
 			);
 			res.status(200).send({
 				ok: true,
